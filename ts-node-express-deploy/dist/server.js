3a8bc54091134c7acc17c4c380caff8132aa486d
WhenSunset <a1018998632@gmail.com>
2022-01-13 18:18:53 +0800

    更新

--- ts-node-express-deploy/dist/server.js
+++ ts-node-express-deploy/dist/server.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var app = (0, express_1.default)();
-var PORT = 3000;
+var PORT = 6631;
 app.use(express_1.default.json());
 app.get('/', function (req, res) {
     res.send('Hello World!');
--- ts-node-express-deploy/server.ts
+++ ts-node-express-deploy/server.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 const app = express()
-const PORT = 3000
+const PORT = 6631 
 
 app.use(express.json())
 
